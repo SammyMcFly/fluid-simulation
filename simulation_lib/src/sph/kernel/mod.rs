0d596b06f1f6b25322ff5c46cd2249a6e4a43bb1
Lysander Treumann <lysander.treumann@gmx.de>
2026-05-26 13:18:40 +0200

    Defines trait for kernel function, Implements cubic b-spline as kernel function (trait), Moves kernel function related code into its own module

---
+++ simulation_lib/src/sph/kernel/mod.rs
@@ -0,0 +1,24 @@
+/// Kernel functions
+///
+use nalgebra::Vector3;
+
+pub mod cubic_spline;
+
+pub trait KernelFn: Send + Sync {
+    /// W(r, h)
+    fn value(distance: f64, smoothing_length: f64) -> f64;
+
+    /// dW/dr (scalar derivative)
+    fn derivative(distance: f64, smoothing_length: f64) -> f64;
+
+    /// Full gradient: (dW/dr) * (r_vec / |r_vec|)
+    ///
+    /// r_vec is defined as the vector from the center of the kernel function to evaluated position.
+    fn gradient(distance_vec: &Vector3<f64>, distance: f64, smoothing_length: f64) -> Vector3<f64> {
+        if distance == 0.0 {
+            Vector3::zeros()
+        } else {
+            distance_vec * (Self::derivative(distance, smoothing_length) / distance)
+        }
+    }
+}
